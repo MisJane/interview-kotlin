ad238f7899d2573c34d96fca78c849cf4b0b899c
Evgeniya <jane.mischenko@gmail.com>
2025-01-29 00:05:05 +0300

    add new UniqueWords

---
+++ src/main/kotlin/app.intech/UniqueAppWords.kt
@@ -0,0 +1,12 @@
+package app.intech
+
+class UniqueAppWords {
+    object UniqueAppWords {
+        fun process(input: List<String?>): List<String> {
+            return input
+                .filterNotNull() // Игнорируем null
+                .map { it.replace(Regex("[aA]"), "4").uppercase() } // Заменяем a/A → 4 и переводим в верхний регистр
+                .distinct() // Уникальные значения
+        }
+    }
+}
---
+++ src/test/kotlin/app/intech/UniqueAppWordsTest.kt
@@ -0,0 +1,23 @@
+package app.intech
+
+
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.containsExactly
+
+class UniqueAppWordsTest {
+    @Test
+    fun `replace a and A with 4, uppercase, unique values`() {
+        val input = listOf("ANNA", "ann4", "-1", null, "test")
+        val result = UniqueAppWords.UniqueAppWords.process(input)
+        expectThat(result).containsExactly("4NN4", "-1", "TEST")
+    }
+
+    @Test
+    fun `duplicates removed after processing`() {
+        val input = listOf("apple", "APPLE", "aPPle")
+        val result = UniqueAppWords.UniqueAppWords.process(input)
+        expectThat(result).containsExactly("4PPLE")
+    }
+
+}
